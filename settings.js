b788f46dca30e2254df7b227019d0f33f7760734
Sarah <frejulian57@gmail.com>
2026-04-24 23:43:07 -0400

    Update Stellar API key in settings.js

--- settings.js
+++ settings.js
@@ -36,7 +36,7 @@ stellar: { url: "https://api.yuki-wabot.my.id", key: "YukiBot-MD" },
 apifaa: { url: "https://api-faa.my.id", key: null },
 xyro: { url: "https://api.xyro.site", key: null },
 yupra: { url: "https://api.yupra.my.id", key: null },
-stellar: { url: "https://api.stellarwa.xyz", key: "" }
+stellar: { url: "https://api.stellarwa.xyz", key: "api-QCQ7L" }
 }
 
 let file = fileURLToPath(import.meta.url)

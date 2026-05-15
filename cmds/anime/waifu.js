94573016cf1fae70e9212e1d4520311652bafdb5
Sarah <frejulian57@gmail.com>
2026-05-15 07:26:00 -0400

    Update waifu command to use new API endpoint

--- cmds/anime/waifu.js
+++ cmds/anime/waifu.js
@@ -6,13 +6,16 @@ export default {
   run: async (client, m, args, usedPrefix, command, text) => {
     try {
       await m.react('🕒')
-      let mode = db.data.chats[m.chat]?.nsfw ? 'nsfw' : 'sfw'
-      let res = await fetch(`https://nekos.best/api/v2/${command}`)
-      if (!res.ok) return
-      let json = await res.json()
-      if (!json.results[0].url) return
+      
+      let mode = db.data.chats[m.chat]?.nsfw ? 'hneko' : command
 
-      let img = Buffer.from(await (await fetch(json.results[0].url)).arrayBuffer())
+      let res = await fetch(`https://nekobot.xyz/api/image?type=${mode}`)
+      if (!res.ok) return
+
+      let json = await res.json()
+      if (!json.message) return
+
+      let img = Buffer.from(await (await fetch(json.message)).arrayBuffer())
 
       await client.sendMessage(m.chat, {
         image: img,
@@ -20,6 +23,7 @@ export default {
       }, { quoted: m })
 
       await m.react('✔️')
+
     } catch (e) {
       await m.react('✖️')
       await m.reply(`> An unexpected error occurred while executing command *${usedPrefix + command}*. Please try again or contact support if the issue persists.\n> [Error: *${e.message}*]`)

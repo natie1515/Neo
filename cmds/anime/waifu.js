e4723936e317191651b1607fdf747917a2d4f54f
Sarah <frejulian57@gmail.com>
2026-05-15 07:28:30 -0400

    Fix API endpoint and update response handling

--- cmds/anime/waifu.js
+++ cmds/anime/waifu.js
@@ -6,16 +6,13 @@ export default {
   run: async (client, m, args, usedPrefix, command, text) => {
     try {
       await m.react('🕒')
-      
-      let mode = db.data.chats[m.chat]?.nsfw ? 'hneko' : command
-
-      let res = await fetch(`https://nekobot.xyz/api/image?type=${mode}`)
+      let mode = db.data.chats[m.chat]?.nsfw ? 'nsfw' : 'sfw'
+      let res = await fetch(`https://nekos.best/api/v2/${command}`)
       if (!res.ok) return
-
       let json = await res.json()
-      if (!json.message) return
+      if (!json.results[0].url) return
 
-      let img = Buffer.from(await (await fetch(json.message)).arrayBuffer())
+      let img = Buffer.from(await (await fetch(json.results[0].url)).arrayBuffer())
 
       await client.sendMessage(m.chat, {
         image: img,
@@ -23,7 +20,6 @@ export default {
       }, { quoted: m })
 
       await m.react('✔️')
-
     } catch (e) {
       await m.react('✖️')
       await m.reply(`> An unexpected error occurred while executing command *${usedPrefix + command}*. Please try again or contact support if the issue persists.\n> [Error: *${e.message}*]`)
